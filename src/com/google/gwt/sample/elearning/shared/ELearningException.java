6c6eb2b904bebf6e1c9df7814023b2e5145027e7
Cristi Pop <cpop@x-iss.com>
2015-11-13 21:59:58 +0200

    Added new exception

---
+++ src/com/google/gwt/sample/elearning/shared/ELearningException.java
@@ -0,0 +1,11 @@
+package com.google.gwt.sample.elearning.shared;
+
+/***
+ * Created by Cristi on 11/13/2015.
+ */
+public class ELearningException extends Exception {
+   public ELearningException(Exception e){
+     super(e);
+   }
+  public ELearningException(){ }
+}
